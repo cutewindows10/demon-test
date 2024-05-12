c96e829aa97be518c13e58ea68989465bf4ffb6d
KiraiEEE <akram.bf.dev@gmail.com>
2024-05-12 14:20:24 +0100

    yes

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,5 +1,6 @@
 import User from '../models/User.js';
 import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
 
 export const getAllUsers = async (req, res) => {
     try {
@@ -113,6 +114,28 @@ export const signUpUser = async (req, res) => {
             branchID
         });
         res.status(201).json({ message: "User registered successfully", userId: newUser.id });
+    } catch (error) {
+        res.status(500).json({ message: "Internal server error" });
+    }
+};
+
+
+
+
+
+export const loginUserWithJWT = async (req, res) => {
+    const { username, password } = req.body;
+    try {
+        const user = await User.findOne({ where: { username } });
+        if (!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
+        const isMatch = password == user.password;
+        if (!isMatch) {
+            return res.status(401).json({ message: "Invalid credentials" });
+        }
+        const token = jwt.sign({ id: user.id, key: user.key }, process.env.JWT_SECRET);
+        res.status(200).json({ message: "Login successful", userId: user.id, token });
     } catch (error) {
         res.status(500).json({ message: "Internal server error" });
     }
