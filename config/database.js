f64cc346f18ec28eece7493fc4edb25289e088ee
KiraiEEE <akram.bf.dev@gmail.com>
2024-05-13 15:48:09 +0100

    a

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 import { Sequelize } from 'sequelize';
-import config from "./config.json" assert{type: 'json'};
+import config from "./config.json" assert {type: 'json'};
 
 const env = process.env.NODE_ENV || 'development'
 
